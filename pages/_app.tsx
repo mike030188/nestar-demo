a1921e6c7929a54bd9652bb3824d3838074677f7
mike030188 <halol.imnida2023@gmail.com>
2024-06-06 05:16:52 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log("App - PAGES ROUTER");
+  // console.log("App - PAGES ROUTER");
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
 
-  return <Component {...pageProps} />;
+  // Socket.io, Redux, Mui...global integration qiliw mumkin
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+// App-> (pageProps.ni)qolgan barcha pages routingda joylawgan filelarga tarqatmoqda props sifatida;
