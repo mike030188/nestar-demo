3704a765db7649c8315608fc4defc44cb4c1e589
mike030188 <halol.imnida2023@gmail.com>
2024-06-08 16:00:39 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // console.log("App - PAGES ROUTER");
