d75d83463fdb0cff9a8eba6607f96d18bba39f45
mike030188 <halol.imnida2023@gmail.com>
2024-06-11 17:32:56 +0900

    feat: property list publishing

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,13 +1,53 @@
+import React, { useState } from "react";
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Stack } from "@mui/material";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
 import { NextPage } from "next";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import Filter from "@/libs/components/property/Filter";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const PropertyList: NextPage = () => {
   // console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
+  const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
 
   return (
-    <div style={{ margin: "20px 0px" }}>
-      <Stack className="container">PROPERTY LIST</Stack>
+    <div id={"property-list-page"} style={{ position: "relative" }}>
+      <Stack className={"container"}>
+        <Box className={"right"}>
+          <span>Sort by</span>
+          <div>
+            <Button endIcon={<KeyboardArrowDownRoundedIcon />}>New</Button>
+          </div>
+        </Box>
+        <Stack className={"propert-page"}>
+          <Stack className={"filter-config"}>
+            <Filter />
+          </Stack>
+
+          <Stack className={"main-config"} mb={"76px"}>
+            <Stack className={"list-config"}>
+              {properties.map((property, index) => {
+                return <PropertyCard key={index} />;
+              })}
+            </Stack>
+
+            <Stack className={"pagination-config"}>
+              <Stack className={"pagination-box"}>
+                <Pagination
+                  page={1}
+                  count={5}
+                  shape="circular"
+                  color="primary"
+                />
+              </Stack>
+
+              <Stack className={"total-result"}>
+                <Typography>Total 6 properties availabel</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 };
